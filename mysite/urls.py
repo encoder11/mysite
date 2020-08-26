847df44f3e40ede8497a333b16f4eddc62717f24
THK <KTH@example.com>
2020-08-26 23:55:32 +0900

    urls fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+#상위 urls로 하위 앱에 있는 화면을 불러올수 있다.
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+#해당 앱에 있는 views파일 안의 index함수를 호출하는 코드
 urlpatterns = [
     path('', views.index, name='index'),
 ]
